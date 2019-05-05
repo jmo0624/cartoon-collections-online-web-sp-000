9f7d9ef5a6a6ceab671eb0b188fee15ee2c3a119
Josh Morris <jmo0624@gmail.com>
2019-05-05 20:39:04 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,8 +11,9 @@ def summon_captain_planet(planeteer_calls)
   end
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? do |call|
+    
 end
 
 def find_the_cheese# code an argument here
